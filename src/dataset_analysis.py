25db9cc47c1cdc93750b1e89a890af5638b3e291
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2026-04-20 11:52:23 +0300

    improve dataset statistics plots

--- src/dataset_analysis.py
+++ src/dataset_analysis.py
@@ -26,6 +26,39 @@ import matplotlib.pyplot as plt
 import util.io
 import util.graphs
 
+MAX_COMMENTS_PER_DISCUSSION = 500
+MAX_CHARACTERS_PER_COMMENT = 500
+
+
+def main(args):
+    util.graphs.seaborn_setup()
+
+    csv_path = Path(args.dataset_path)
+    graph_dir = Path(args.graph_dir)
+
+    print("Loading dataset to extract statistics...")
+    df = util.io.progress_load_csv(csv_path)
+    df = df[df.dataset != "vmd"]
+    df.dataset = df.dataset.replace(
+        {
+            "ceri": "CeRI",
+            "wikidisputes": "WikiDisputes",
+            "wikiconv": "WikiConv",
+            "fora": "Fora",
+            "umod": "UMod",
+            "wikitactics": "WikiTactics",
+            "whow": "WHoW",
+            "iq2": "IQ2",
+            "cmv_awry": "CMV-Awry",
+        }
+    )
+
+    print(f"Dataset total size: {convert_bytes(csv_path.stat().st_size)}")
+
+    words_per_comment_plot(df, graph_dir)
+    comments_per_discussion_plot(df, graph_dir)
+    moderation_plot(df[df.dataset != "UMod"], graph_dir=graph_dir)
+
 
 def convert_bytes(num):
     """
@@ -44,9 +77,10 @@ def comments_per_discussion_plot(df: pd.DataFrame, graph_dir: Path) -> None:
         .reset_index(name="comments_per_disc")
     )
 
-    # Cap at 200 because there is a tail going up to 1200 comments
+    # Cap at 2MAX_COMMENT_LEN_CHARS because there is a
+    # tail going up to 1200 comments
     disc_sizes["comments_per_disc"] = disc_sizes["comments_per_disc"].clip(
-        upper=200
+        upper=MAX_COMMENTS_PER_DISCUSSION
     )
 
     plt.figure(figsize=(8, 5))
@@ -59,7 +93,10 @@ def comments_per_discussion_plot(df: pd.DataFrame, graph_dir: Path) -> None:
         bins=40,  # do NOT let this go to auto
     )
     plt.title("Distribution of Comments per Discussion (Density)")
-    plt.xlabel("Number of Comments per Discussion (capped at 200)")
+    plt.xlabel(
+        "Number of Comments per Discussion "
+        f"(capped at {MAX_COMMENTS_PER_DISCUSSION})"
+    )
     plt.ylabel("Density")
     plt.tight_layout()
 
@@ -105,63 +142,23 @@ def words_per_comment_plot(df: pd.DataFrame, graph_dir: Path) -> None:
     )
 
     # Cap long tail (optional but very useful for readability)
-    df["words_per_comment"] = df["words_per_comment"].clip(upper=200)
+    df["words_per_comment"] = df["words_per_comment"].clip(upper=MAX_CHARACTERS_PER_COMMENT)
 
     plt.figure(figsize=(8, 5))
     sns.histplot(
         data=df,
         x="words_per_comment",
         hue="dataset",
-        bins=50, # do NOT let this go to auto
+        bins=50,  # do NOT let this go to auto
         stat="density",
         common_norm=False,
     )
     plt.title("Distribution of Words per Comment (Density)")
-    plt.xlabel("Words per Comment (capped at 200)")
+    plt.xlabel(F"Words per Comment (capped at {MAX_CHARACTERS_PER_COMMENT})")
     plt.ylabel("Density")
     plt.tight_layout()
 
     util.graphs.save_plot(graph_dir / "analysis_words_per_comment.png")
-
-
-def main(args):
-    util.graphs.seaborn_setup()
-
-    csv_path = Path(args.dataset_path)
-    graph_dir = Path(args.graph_dir)
-
-    print("Loading dataset to extract statistics...")
-    df = util.io.progress_load_csv(csv_path)
-
-    """
-    print("*" * 25)
-    print("Comments per discussion:")
-    print(df.groupby("conv_id").size().describe())
-
-    print("*" * 25)
-    print("Comments per dataset:")
-    print(df.groupby("dataset")["message_id"].nunique())
-
-    print("*" * 25)
-    print("Discussions per dataset:")
-    print(df.groupby("dataset")["conv_id"].nunique())
-
-    print("*" * 25)
-    print("Word count per comment:")
-    print(
-        df.text.astype(str)
-        .apply(lambda x: x.split())
-        .apply(len)
-        .astype(int)
-        .describe()
-    )
-    """
-    print("*" * 25)
-    print(f"Dataset total size: {convert_bytes(csv_path.stat().st_size)}")
-
-    words_per_comment_plot(df, graph_dir)
-    comments_per_discussion_plot(df, graph_dir)
-    moderation_plot(df, graph_dir=graph_dir)
 
 
 if __name__ == "__main__":
